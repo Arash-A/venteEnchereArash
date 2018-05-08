a206de63b4f05547839b217d628d7b65f914e7e3
Sasha Bouchard <sabo357@hotmail.com>
2018-05-08 17:26:17 -0400

    PagedList des objets avec triage et recherche par nom

--- venteTest/Controllers/HomeController.cs
+++ venteTest/Controllers/HomeController.cs
@@ -22,10 +22,73 @@ namespace venteTest.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
-        {
-            var applicationDbContext = _context.Objets.Include(o => o.Categorie);
-            return View(await applicationDbContext.ToListAsync());
+        public async Task<IActionResult> Index(
+            string sortOrder,
+            string currentFilter,
+            string searchString,
+            int? page) { 
+
+            //var applicationDbContext = _context.Objets.Include(o => o.Categorie);
+
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : ""; // name asc
+            ViewData["PrixDepartSortParm"] = sortOrder == "prix_asc" ? "prix_desc" : "prix_asc";
+            ViewData["DateInscritSortParm"] = sortOrder == "date_asc" ? "date_desc" : "date_asc";
+            ViewData["DureeMiseVenteSortParm"] = sortOrder == "duree_asc" ? "duree_desc" : "duree_asc";
+            ViewData["CategorieSortParm"] = sortOrder == "categ_asc" ? "categ_desc" : "categ_asc";
+
+            if (searchString != null) {
+                page = 1;
+            } else {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+
+            var objets = from o in _context.Objets.Include(o => o.Categorie)
+                           select o;
+
+            if (!String.IsNullOrEmpty(searchString)) {
+                objets = objets.Where(s => s.Nom.Contains(searchString)
+                || s.Description.Contains(searchString));
+            }
+
+
+            switch (sortOrder) {
+                case "name_desc":
+                    objets = objets.OrderByDescending(s => s.Nom);
+                    break;
+                case "prix_asc":
+                    objets = objets.OrderBy(s => s.PrixDepart);
+                    break;
+                case "prix_desc":
+                    objets = objets.OrderByDescending(s => s.PrixDepart);
+                    break;
+                case "date_asc":
+                    objets = objets.OrderBy(s => s.DateInscription);
+                    break;
+                case "date_desc":
+                    objets = objets.OrderByDescending(s => s.DateInscription);
+                    break;
+                case "duree_asc":
+                    objets = objets.OrderBy(s => s.DureeMiseVente);
+                    break;
+                case "duree_desc":
+                    objets = objets.OrderByDescending(s => s.DureeMiseVente);
+                    break;
+                case "categ_asc":
+                    objets = objets.OrderBy(s => s.Categorie.Nom);
+                    break;
+                case "categ_desc":
+                    objets = objets.OrderByDescending(s => s.Categorie.Nom);
+                    break;
+                default: // name asc
+                    objets = objets.OrderBy(s => s.Nom);
+                    break;
+            }
+
+            int pageSize = 3;
+            //return View(await objets.AsNoTracking().ToListAsync());
+            return View(await PaginatedList<Objet>.CreateAsync(objets.AsNoTracking(), page ?? 1, pageSize));
         }
 
         public IActionResult About()
