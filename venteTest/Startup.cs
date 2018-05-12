e571dd9145539eab09f5730daf9713f1d7184716
Sasha Bouchard <sabo357@hotmail.com>
2018-05-12 16:45:56 -0400

    Partie administrateur gestion categories, ajout plugin AutoMapper (voir classe AutoMapperConfig pour ajouter nos mappings)

--- venteTest/Startup.cs
+++ venteTest/Startup.cs
@@ -84,7 +84,10 @@ namespace venteTest
             });
 
             //ajout sb pour créer admin et rôles (Si requis)
-            CreateRolesAdminUsers(serviceProvider).Wait();
+            //CreateRolesAdminUsers(serviceProvider).Wait();
+
+            // Ajout SB pour faire nos mappings entre Model et ViewModels
+            AutoMapperConfig.RegisterMappings();
 
             // Ajour Arash pour Hangfire
             app.UseHangfireServer();
@@ -165,4 +168,32 @@ namespace venteTest
         }
         // FIN AJOUT SB
     }
+
+    public class AutoMapperConfig {
+        // 
+        public static void RegisterMappings() {
+
+            AutoMapper.Mapper.Initialize(cfg => {            
+                cfg.CreateMap<Categorie, Models.AdminViewModels.CategorieViewModel>();
+                cfg.CreateMap<Models.AdminViewModels.CategorieViewModel, Categorie>();
+                                                   
+                                                              
+            });                                                                                                        
+
+            // Exemples utilisés dans le contrôleur :
+            //
+            //Ex1 pour mapper:  
+            // Article article = _articleManager.lstArticles.FirstOrDefault(p => p.Titre.Equals(titre));
+            // ArticleViewModel model = Mapper.Map<Article, ArticleViewModel>(article); // conversion d'une entité Article en ArticleViewModel
+            //return View(model)
+
+            //Ex2 pour mapper:
+            // IList<Article> lArt = _articleManager.lstArticles;
+            // IList<ArticleViewModel> model = Mapper.Map<IList<Article>, IList<ArticleViewModel>>(lArt);
+            // return PartialView(model);
+
+        }
+
+    }
+
 }
