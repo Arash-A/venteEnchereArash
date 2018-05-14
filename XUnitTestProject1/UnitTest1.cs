5b9805a287c57450521459f9b5cc914990ff6444
Ren S. Roy <ren.s.roy@gmail.com>
2018-05-13 23:27:45 -0400

    Localisation
    Ajout de ressources pour la localisation

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -6,6 +6,7 @@ namespace XUnitTestProject1
 {
     public class UnitTest1
     {
+        //TESTS WILL NEED TO BE ADAPTED TO THE FOLLOWING CONTROLLERS:
         //[Fact]
         //public void Test1()
         //{
