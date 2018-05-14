9c6c1c42e34c0b62dcc53446b90fb2f818b0cf00
Ren S. Roy <ren.s.roy@gmail.com>
2018-05-14 00:46:41 -0400

    updates localisation
    views/account

--- venteTest/Resources/Views/StringsAccount.Designer.cs
+++ venteTest/Resources/Views/StringsAccount.Designer.cs
@@ -59,5 +59,176 @@ namespace venteTest.Resources.Views {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Accès refusé.
+        /// </summary>
+        public static string AccesRefuse {
+            get {
+                return ResourceManager.GetString("AccesRefuse", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Vous n&apos;avez pas accès à la ressource..
+        /// </summary>
+        public static string AccesRefuseDescription {
+            get {
+                return ResourceManager.GetString("AccesRefuseDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to compte..
+        /// </summary>
+        public static string AssocierCompte {
+            get {
+                return ResourceManager.GetString("AssocierCompte", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Associez votre.
+        /// </summary>
+        public static string AssocierSon {
+            get {
+                return ResourceManager.GetString("AssocierSon", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Vous vous êtes autentifié avec succès avec.
+        /// </summary>
+        public static string AuthentificationReussie {
+            get {
+                return ResourceManager.GetString("AuthentificationReussie", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to S&apos;enregistrer.
+        /// </summary>
+        public static string BoutonRegister {
+            get {
+                return ResourceManager.GetString("BoutonRegister", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Envoyer.
+        /// </summary>
+        public static string BoutonSubmit {
+            get {
+                return ResourceManager.GetString("BoutonSubmit", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Veuillez cliquer ici pour vous connecter..
+        /// </summary>
+        public static string ClickToLogin {
+            get {
+                return ResourceManager.GetString("ClickToLogin", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Confirmation de courriel.
+        /// </summary>
+        public static string ConfirmerCourriel {
+            get {
+                return ResourceManager.GetString("ConfirmerCourriel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Merci de confirmer votre courriel..
+        /// </summary>
+        public static string ConfirmerCourrielDescription {
+            get {
+                return ResourceManager.GetString("ConfirmerCourrielDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Créer un nouvelle compte.
+        /// </summary>
+        public static string CreerCompte {
+            get {
+                return ResourceManager.GetString("CreerCompte", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Veuillez entrer une adresse courriel pour le site ci-dessous et cliquez le bouton s&apos;enregistrer pour compléter la connexion..
+        /// </summary>
+        public static string EntrerCourriel {
+            get {
+                return ResourceManager.GetString("EntrerCourriel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Entrez votre courriel..
+        /// </summary>
+        public static string EntrerCourrielST {
+            get {
+                return ResourceManager.GetString("EntrerCourrielST", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Mot de passe oublié?.
+        /// </summary>
+        public static string MDPOublie {
+            get {
+                return ResourceManager.GetString("MDPOublie", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Votre mot de passe a été réinitialisé..
+        /// </summary>
+        public static string MDPReinitialise {
+            get {
+                return ResourceManager.GetString("MDPReinitialise", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to S&apos;enregistrer.
+        /// </summary>
+        public static string Register {
+            get {
+                return ResourceManager.GetString("Register", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Vous êtes déconnectés..
+        /// </summary>
+        public static string SignedOut {
+            get {
+                return ResourceManager.GetString("SignedOut", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Vous vous êtes déconnecté avec succès..
+        /// </summary>
+        public static string SignedOutDescription {
+            get {
+                return ResourceManager.GetString("SignedOutDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Veuillez vérifier votre courriel pour réinitialiser votre mot de passe..
+        /// </summary>
+        public static string VerifierCourriel {
+            get {
+                return ResourceManager.GetString("VerifierCourriel", resourceCulture);
+            }
+        }
     }
 }
