3244742d81ef54e7c53af9e021bc7d5e543cbffd
Arash <amiri2281@gmail.com>
2018-05-16 20:58:14 -0400

    Objet ajouté va étre vendu après la dateLimite

---
+++ venteTest/Services/FiniVenteObjet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using venteTest.Data;
+using venteTest.Models;
+
+namespace venteTest.Services
+{
+    public class FiniVenteObjet
+    {
+        private readonly ApplicationDbContext _context;
+       
+
+        public FiniVenteObjet(ApplicationDbContext db) {
+            _context = db;
+          
+        }
+
+        public void ChangeStatus(int objetId) {
+            var objet = _context.Objets.First(a => a.ObjetID == objetId);
+            objet.Status = Status.Vendu;
+            _context.SaveChangesAsync();
+        }
+    }
+}
